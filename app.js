cf4a26322112b1e2741c4c053602169b457e8a9e
Lucas Cristin <lukicristin@gmail.com>
2021-12-23 12:38:50 -0300

    Se agregaron los numeros ganadores :computer:

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //-------MIS NUMEROS DEL SORTEO
 const numerosGanadores = [
-    44 , 55 , 15
+    15837, 14449, 11183, 11596, 14486, 14341, 13454, 5413, 4235, 14846, 14751, 6393, 6316, 5104, 6252, 6137, 8986, 
+    6497, 5383, 5257, 5212, 4277, 8674, 14817, 8877, 2320, 2823, 2645, 0074, 0024, 6865, 6836, 6775, 6613, 6742, 
+    1121, 5549, 2069, 0307, 4119, 4087, 4997, 3536, 8591, 4603, 4856, 4828, 13927, 10611, 10739, 10780, 11201, 13404,
+    14379, 15532, 15300
 ];
 
 //-----DESARROLLO
@@ -19,4 +22,4 @@ btn.addEventListener('click', () => {
     swal("No ganaste mala ahi","no ganaste la proxima sera", "error")    
 });
 
-console.log(numerosGanadores);
+console.log(numerosGanadores.length);
